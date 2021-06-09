ade514adbcb1f47c86d471a2ad6b16e2770b0769
Umberto Michelucci <umberto.michelucci@gmail.com>
2021-06-09 15:00:43 +0200

    .

--- code/version_check.py
+++ code/version_check.py
@@ -2,6 +2,12 @@ import subprocess
 import sys
 import tensorflow as tf
 
+print("----------------------------------------------------")
 print("TensorFlow Version:", tf.__version__)
+print("----------------------------------------------------")
 command = subprocess.run('/usr/local/cuda/bin/nvcc --version', capture_output=True, shell = True    )
 sys.stdout.buffer.write(command.stdout)
+print("----------------------------------------------------")
+command = subprocess.run('nvidia-smi', capture_output=True, shell = True    )
+sys.stdout.buffer.write(command.stdout)
+print("----------------------------------------------------")
