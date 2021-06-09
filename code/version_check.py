607df0c9c8df553eef057f094dc8669b9ffcf678
Umberto Michelucci <umberto.michelucci@gmail.com>
2021-06-09 14:59:18 +0200

    .

--- code/version_check.py
+++ code/version_check.py
@@ -1,6 +1,7 @@
 import subprocess
+import sys
 import tensorflow as tf
 
 print("TensorFlow Version:", tf.__version__)
-command = subprocess.run('/usr/local/cuda/bin/nvcc --version', capture_output=True, shell = True)
+command = subprocess.run('/usr/local/cuda/bin/nvcc --version', capture_output=True, shell = True    )
 sys.stdout.buffer.write(command.stdout)
