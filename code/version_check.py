12e6436719deda8f145c3a5d83db21b4c53d4067
Umberto Michelucci <umberto.michelucci@gmail.com>
2021-06-09 15:05:30 +0200

    Added comments and version

--- code/version_check.py
+++ code/version_check.py
@@ -1,3 +1,15 @@
+#
+# Version Checker
+#
+# This script check the versions of TensorFlow, cuda, cuDNN and
+# the NVIDIA driver.
+#
+# Version 1.0
+# Author: Umberto Michelucci, umberto.michleucci@toelt.ai
+# (C) TOELT LLC 2021
+#
+
+
 import subprocess
 import sys
 import tensorflow as tf
