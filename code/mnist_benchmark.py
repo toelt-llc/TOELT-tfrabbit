0e4c4a5e82c66623be1b32f323f89eaeb3ae7eab
Umberto Michelucci <umberto.michelucci@gmail.com>
2021-06-10 11:06:58 +0200

    Added MNIST benchmarking

---
+++ code/mnist_benchmark.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import time
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(5000,activation='relu'),
+  tf.keras.layers.Dense(5000,activation='relu'),
+  tf.keras.layers.Dense(5000,activation='relu'),
+  tf.keras.layers.Dense(10)
+])
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+start = time.time()
+model.fit(
+    ds_train,
+    epochs=6,
+    validation_data=ds_test,
+)
+end = time.time()
+print("-------------------------------------------------")
+print("Benachmark Results for VGG19")
+print()
+print("Elapsed Time (min):",(end - start)/60.0)
+print("-------------------------------------------------")
