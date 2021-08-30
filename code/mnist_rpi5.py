21bae585852d01355e4e31165577a6d10d1aa3f6
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-08-30 13:11:03 +0200

    results folder update

--- code/mnist_rpi5.py
+++ code/mnist_rpi5.py
@@ -72,7 +72,7 @@ def main(argv):
 
     dfres.index.name = 'Neurons'
     ## !!! to change depending on the device
-    dfres.to_csv('./saved_results/frodo/'+ result + '.csv')
+    dfres.to_csv('./saved_results/'+ result + '.csv')
     #dfres.to_pickle('./saved_results/' + result + '.pkl')
     print(dfres)
     print('Prediction time is over {} testing examples. '.format(predictions))
