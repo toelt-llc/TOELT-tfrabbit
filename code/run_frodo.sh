b7c259e300a6f45d625dec08c50b7d840486f87a
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-01 15:25:51 +0200

    ^

--- code/run_frodo.sh
+++ code/run_frodo.sh
@@ -5,6 +5,6 @@ do
     echo 'Run num '${i}' of '${runs}
     echo '_________________________'
     string = "frodo"${i}
-    python3 mnist_rpi5.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3250,3500,3750,4096,4500 -r $string
+    python3 mnist_rpi6.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3250,3500,3750,4096,4500 -r $string
 done
 echo 'Runs completed : '${runs}'/'${runs}
