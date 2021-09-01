50df2f226818d4345e41887255906be8edfa439c
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-01 08:12:30 +0200

    run pi update (*10)

--- code/run_pi.sh
+++ code/run_pi.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python3 mnist_rpi4.py -n 1500 -p 10000 -r pi3
+for ((i=1; i<=10; i++))
+do  
+    string = "pi"${i}
+    python3 mnist_rpi5.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3250,3500,3750,4096,4500 -p 10000 -r string
+done
