38130ddbf35ca91c198374bc7f77121972c9a05a
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-02 08:07:23 +0200

    frodo up to 10k neurons

--- code/tests/bashloop.sh
+++ code/tests/bashloop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-declare -i runs=2
+declare -i runs=3
 for ((i=1; i<= ${runs}; i++))
 do  
     echo 'Run num '${i}' of '${runs}
     echo '_________________________'
     string="mactodel"${i}
-    python3 mnist_rpi6.py -n 5 -l 5 -r $string
+    python3 mnist_rpi6.py -n 5,10,15 -l 5,6,7 -r $string
     #echo $string
 done
 echo 'Runs completed : '${runs}'/'${runs}
