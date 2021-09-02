da5a491a7c0230cfdf0b9ad8db4de3e4635b5f9c
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-02 13:34:16 +0200

    run pi long

--- code/run_pi.sh
+++ code/run_pi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-declare -i runs=10
+declare -i runs=2
 for ((i=1; i<=${runs}; i++))
 do  
     echo 'Run num '${i}' of '${runs}
     echo '_________________________'
-    string = "pi"${i}
-    python3 mnist_rpi6.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3250,3500,3750,4096,4500 -r $string
+    string="pi/longpi"${i}
+    python3 mnist_rpi5.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000 -r $string
 done
 echo 'Runs completed : '${runs}'/'${runs}
