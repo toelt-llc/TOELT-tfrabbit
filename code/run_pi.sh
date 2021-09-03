3e2b98e75ba755d2fdab0bce0e89fcce80566c91
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-03 08:41:49 +0200

    7000 thousands pi

--- code/run_pi.sh
+++ code/run_pi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-declare -i runs=2
+declare -i runs=1
 for ((i=1; i<=${runs}; i++))
 do  
     echo 'Run num '${i}' of '${runs}
     echo '_________________________'
     string="pi/longpi"${i}
-    python3 mnist_rpi5.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000 -r $string
+    python3 mnist_rpi5.py -n 7000 -r $string
 done
 echo 'Runs completed : '${runs}'/'${runs}
