1138c74bc0a364f7ee683f047caa05865fc08b1b
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-08 10:49:02 +0200

    ReadMEs updates + run_frodo fashion_mnist

--- code/run_frodo.sh
+++ code/run_frodo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #TODO, short explanation
 
-declare -i runs=1
+declare -i runs=3
 for ((i=1; i<=${runs}; i++))
 do  
     echo 'Run num '${i}' of '${runs}
     echo '_________________________'
-    string="/frodo/mnist7_test"${i}
-    python3 mnist_rpi7.py -n 250 -l 2 -r $string
+    string="/frodo/batch3_"${i}
+    python3 mnist_rpi7.py -n 5,10,50,256,512,1024,1500,2048,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000 -l 2,4,6,8 -r $string
 done
 echo 'Runs completed : '${runs}'/'${runs}
