5ddbf8ff3ccc11a0cfc0fe7c188a0fdebda86011
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-09-15 10:57:48 +0200

    batch4 results

--- code/run_frodo.sh
+++ code/run_frodo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #TODO, short explanation
-#TODO, loop over different parameters
+#TO(maybe)DO, loop over different parameters
 
-declare -i runs=3
+declare -i runs=10
 for ((i=1; i<=${runs}; i++))
 do  
     echo 'Run num '${i}' of '${runs}
     echo '_________________________'
-    string="/frodo/batch3_"${i}
+    string="/frodo/batch4_"${i}
     #check the mnist_rpi version
-    python3 mnist_rpi7.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000 -l 2,4,6,8 -r $string
+    python3 mnist_rpi8.py -n 5,10,50,128,256,512,1024,1500,2048,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000 -l 2,4,6,8 -r $string -d mnist
 done
 echo 'Runs completed : '${runs}'/'${runs}
