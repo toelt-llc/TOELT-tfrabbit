ce64233bda7e22e21854eea123e891da69a77932
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-10-13 13:11:43 +0200

    accuracy script rdy for frodo

--- code/mnist_rpiv2_accuracy.py
+++ code/mnist_rpiv2_accuracy.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import time 
 import pathlib
+import pickle
 import sys
 import os
 
@@ -25,10 +26,16 @@ for _,_,models in os.walk('./progressive_models/'):
         dic[model] = []
         start = time.time()
         loaded_model = tf.keras.models.load_model('./progressive_models/'+model)
-
+        loss,acc = loaded_model.evaluate(test_images, test_labels)
+        # loss = round(eval[0],2)
+        # acc = round(eval[1],2)
         end = time.time()
-        dic[model].append(round(end-start,2))
+
+        dic[model].append(acc)
+        #dic[model].append(round(end-start,2))
 
 dfres = pd.DataFrame.from_dict(dic)
 print(dfres)
 dfres.to_csv('./saved_results/fnn_inferences/accuracy_h5.csv')
+with open('./saved_results/fnn_inferences/accuracy_dict.pkl', 'wb') as f:
+    pickle.dump(dic, f)
