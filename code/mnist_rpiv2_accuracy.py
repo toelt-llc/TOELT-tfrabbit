0b1869aa5f750d429a5b63a2b864f2785494dc2b
a-gucciardi <arnaud.gucciardi@toelt.ai>
2021-10-13 13:03:13 +0200

    accuracies notebook

---
+++ code/mnist_rpiv2_accuracy.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import time 
+import pathlib
+import sys
+import os
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+classic_models_dir = pathlib.Path('./progressive_models')
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.astype(np.float32) / 255.0
+test_images = test_images.astype(np.float32) / 255.0
+
+# Run all kept h5 models, to have an idea on accuracy changes.
+
+dic = {} #Save the converting times
+for _,_,models in os.walk('./progressive_models/'):
+    for model in sorted(models):
+        dic[model] = []
+        start = time.time()
+        loaded_model = tf.keras.models.load_model('./progressive_models/'+model)
+
+        end = time.time()
+        dic[model].append(round(end-start,2))
+
+dfres = pd.DataFrame.from_dict(dic)
+print(dfres)
+dfres.to_csv('./saved_results/fnn_inferences/accuracy_h5.csv')
